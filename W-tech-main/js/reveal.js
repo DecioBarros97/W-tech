dc5bc54dbfe810062cea5386fec1e19454573f33
unknown <deciobarros97@gmail.com>
2025-03-04 17:29:16 -0300

    Resolvendo conflitos

---
+++ W-tech-main/js/reveal.js
@@ -0,0 +1,40 @@
+
+window.revelar = ScrollReveal({reset:true})
+
+// ---- Topo Site ----
+
+revelar.reveal ('.efeito-txt-topo' , 
+{
+    reset:false,
+    duration: 1000, 
+    distance: '90px'
+})
+revelar.reveal ('.efeito-txt-topo2' , 
+    {
+        reset:false,
+        duration: 1000, 
+        distance: '90px',
+        delay: 1000
+    })
+
+// ---- Section2 ----
+revelar.reveal ('.efeito-txt-section2' , 
+    {
+        reset:false,
+        duration: 1000, 
+        distance: '90px'
+    })
+    revelar.reveal ('.efeito-img-section2' , 
+        {
+            reset:false,
+            duration: 1000, 
+            distance: '90px',
+            delay: 1000
+        }) 
+
+        revelar.reveal ('.efeito-txt-section3' , 
+            {
+                reset:false,
+                duration: 1000, 
+                distance: '90px'
+            }) 
--- js/reveal.js
+++ js/reveal.js
@@ -21,23 +21,35 @@ revelar.reveal ('.efeito-txt-topo2' ,
 revelar.reveal ('.efeito-txt-section2' , 
     {
         reset:false,
+<<<<<<< HEAD
         origin: 'left',
+=======
+>>>>>>> 270fe70 (commit primeiro)
         duration: 1000, 
         distance: '90px'
     })
     revelar.reveal ('.efeito-img-section2' , 
         {
             reset:false,
+<<<<<<< HEAD
             origin: 'right',
             duration: 2000, 
             distance: '190px'
             
+=======
+            duration: 1000, 
+            distance: '90px',
+            delay: 1000
+>>>>>>> 270fe70 (commit primeiro)
         }) 
 
         revelar.reveal ('.efeito-txt-section3' , 
             {
                 reset:false,
+<<<<<<< HEAD
                 origin: 'right',
+=======
+>>>>>>> 270fe70 (commit primeiro)
                 duration: 1000, 
                 distance: '90px'
             }) 
