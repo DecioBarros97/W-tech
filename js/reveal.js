044d90888ada446e3e7d3e6db3cfce1af418978e
Décio Matheus Aconsoerde Barros <deciobarros97@gmail.com>
2025-03-06 11:06:58 -0300

    commit modifica

--- js/reveal.js
+++ js/reveal.js
@@ -1,55 +1,40 @@
 
-window.revelar = ScrollReveal({reset:true})
+window.revelar = ScrollReveal({ reset: true })
 
 // ---- Topo Site ----
 
-revelar.reveal ('.efeito-txt-topo' , 
-{
-    reset:false,
-    duration: 1000, 
-    distance: '90px'
-})
-revelar.reveal ('.efeito-txt-topo2' , 
+revelar.reveal('.efeito-txt-topo',
     {
-        reset:false,
-        duration: 1000, 
+        reset: false,
+        duration: 1000,
+        distance: '90px'
+    })
+revelar.reveal('.efeito-txt-topo2',
+    {
+        reset: false,
+        duration: 1000,
         distance: '90px',
         delay: 1000
     })
 
 // ---- Section2 ----
-revelar.reveal ('.efeito-txt-section2' , 
+revelar.reveal('.efeito-txt-section2',
     {
-        reset:false,
-<<<<<<< HEAD
-        origin: 'left',
-=======
->>>>>>> 270fe70 (commit primeiro)
-        duration: 1000, 
+        reset: false,
+        duration: 1000,
         distance: '90px'
     })
-    revelar.reveal ('.efeito-img-section2' , 
-        {
-            reset:false,
-<<<<<<< HEAD
-            origin: 'right',
-            duration: 2000, 
-            distance: '190px'
-            
-=======
-            duration: 1000, 
-            distance: '90px',
-            delay: 1000
->>>>>>> 270fe70 (commit primeiro)
-        }) 
+revelar.reveal('.efeito-img-section2',
+    {
+        reset: false,
+        duration: 1000,
+        distance: '90px',
+        delay: 1000
+    })
 
-        revelar.reveal ('.efeito-txt-section3' , 
-            {
-                reset:false,
-<<<<<<< HEAD
-                origin: 'right',
-=======
->>>>>>> 270fe70 (commit primeiro)
-                duration: 1000, 
-                distance: '90px'
-            }) 
+revelar.reveal('.efeito-txt-section3',
+    {
+        reset: false,
+        duration: 1000,
+        distance: '90px'
+    }) 
